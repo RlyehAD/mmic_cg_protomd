f42ede505c0aee8c549ce88b1f3edd001274cfbc
Xu_Guo <guo12@iu.edu>
2021-07-28 17:21:45 -0400

    construct the io models

--- mmic_cg_protomd/models/io.py
+++ mmic_cg_protomd/models/io.py
@@ -1,19 +1,27 @@
-from cmselemental.models.procedures import ProcInput, ProcOutput
+from cmselemental.models.procedures import ProcInput
 from mmelemental.models.base import ProtoModel
-from mmelemental.models import Molecule, ForceField, TrajInput, ForcesInput
-from pydantic import Field, validator
-from typing import Optional, Dict, List, Tuple, Union
+from mmic_cg.models import CoarseInput, CoarseOutput
+from mmelemental.models import Molecule
+from pydantic import Field
+
 
 __all__ = ["InputModel", "OutputModel"]
 
 
-class InputModel(ProtoModel):
+class ComputeProtomdInput(ProcInput):
     """ An input model for mmic_cg_protomd. """
+    proc_input: CoarseInput = Field(..., description="Procedure input schema.")
 
-    ...
+    molecule: str = Field(
+        ...,
+        description="The file of the coordinates of the atoms in the system. Should be a .gro file.",
+    )
 
 
-class OutputModel(ProtoModel):
+
+class ComputeProtomdOutput(ProtoModel):
     """ An output model for mmic_cg_protomd. """
 
-    ...
+    proc_input: CoarseOutput = Field(..., description="Procedure input schema.")
+
+    molecule: str = Field(..., description="Molecule file string object that stores the coarse grained molecules")
