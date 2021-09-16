e12ca61fa8e77d04428e43d5c200ff9db1920d72
Xu_Guo <guo12@iu.edu>
2021-09-16 18:00:53 -0400

    fix import bugs

--- mmic_cg_protomd/components/protomd_compute_component.py
+++ mmic_cg_protomd/components/protomd_compute_component.py
@@ -3,10 +3,11 @@ Components in mmic_cg_protomd.
 """
 import mmelemental
 import numpy as np 
+import os
 import MDAnalysis as mda 
 import proto_md.subsystems as ss
 #from mmic_cg_protomd.models import ComputeProtomdInput, ComputeProtomdOutput
-from mmelemental.util.files import random_file
+from cmselemental.util.files import random_file
 from mmic_cg.models.proc import CoarseInput, CoarseOutput
 from typing import List, Tuple, Optional
 from mmic.components.blueprints.generic_component import GenericComponent
@@ -54,7 +55,8 @@ class CoarseProtoMDComponent(GenericComponent):
         # write the gro file
         mols = inputs.molecule
         mol_name, mol = list(mols.items()).pop()
-        gro_file = random_file(suffix=".gro") 
+        gro_file = random_file(suffix=".gro")
+        clean_files = [gro_file] 
         mol.to_file(gro_file, translator="mmic_parmed")
 
         cg_compute = ComputeProtomdInput(
@@ -88,6 +90,8 @@ class CoarseProtoMDComponent(GenericComponent):
             j = j+1
 
 
+        self.cleanup(clean_files)
+        
         return True, CoarseOutput(
             proc_input=inputs,
             schema_name=inputs.schema_name,
@@ -95,3 +99,12 @@ class CoarseProtoMDComponent(GenericComponent):
             molecule=mols,
             success=True,
             )
+
+
+    @staticmethod
+    def cleanup(remove: List[str]):
+        for item in remove:
+            if os.path.isdir(item):
+                shutil.rmtree(item)
+            elif os.path.isfile(item):
+                os.remove(item)
